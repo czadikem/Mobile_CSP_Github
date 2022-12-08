e11f8371a0828cfd2240a40a476fba2f36c80f83
Autiboy <czadikem@readytodream.com>
2022-12-08 09:13:11 -0700

    Add Files

---
+++ MAUTI/program-y/bots/mauti/scripts/xnix/template-y-rest.sh
@@ -0,0 +1,13 @@
+
+#! /bin/sh
+
+clear
+
+export PYTHONPATH=../../../../src
+
+if [ -z "${PYTHONPATH}" ]; then
+  echo "PYTHONPATH is undefined"
+  exit
+fi
+
+python3 -m programy.clients.restful.flask.client --config ../../config/xnix/config.flask.rest.yaml --cformat yaml --logging ../../config/xnix/logging.yaml
